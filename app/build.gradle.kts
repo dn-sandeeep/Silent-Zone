6956576ad0d8177c8c568e5a37711e94ef15ba08
sandeep chauhan <sandeepchauhan893@gmail.com>
2026-04-12 23:12:19 +0530

    versioncode change for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.sandeep.silentzone"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
