c158f876dd51c1ef1d7f180241d8ae4836ffb99a
sandeep chauhan <sandeepchauhan893@gmail.com>
2026-04-28 23:39:12 +0530

    versionCode upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.sandeep.silentzone"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.0.6"
+        versionCode = 10
+        versionName = "0.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
